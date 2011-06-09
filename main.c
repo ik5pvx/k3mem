33bc94d48156f288ce6b1d7224ae14948893587e
Pierfrancesco Caci <pf@tippete.net>
2011-06-09 22:57:07 +0200

    Introduce use of long options. 		  compiles but does not work with long options yet.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <string.h>
-#include <unistd.h>
+#include <getopt.h>
 #include <stdlib.h>
 #include "erCommandInfo.h"
 #include "k3comms.h"
@@ -10,10 +10,10 @@ void retrieveAddress(char * addr);
 main(int argc, char *argv[]) {
 
 	char *addr, c, cmd[9], *response;
-	int gotBrief = 0,
-	    gotMemChoice = 0,
-	    gotMemIndex = 0,
-		gotRaw = 0,
+	int gotBrief = 0,         /* -b */
+	    gotMemChoice = 0,     /* -m */
+	    gotMemIndex = 0,      /* -i */
+		gotRaw = 0,           /* -r */
 		i, j,
 		index;
 	k3FreqMemInfo *memInfo;
@@ -21,8 +21,31 @@ main(int argc, char *argv[]) {
 	memInfo = newK3FreqMemInfo();
 
 	if (argc <= 1) usage(argv[0]);
-	while ((c = getopt(argc, argv, "a:bimrs:")) != EOF) {
+	while (1) {
+		int option_index = 0;
+		static struct option long_options[] = {
+			{"address",required_argument,0,0},
+			{"brief",optional_argument,0,0},
+			{"index",optional_argument,0,0},
+			{"memset",optional_argument,0,0},
+			{"raw",optional_argument,0,0},
+			{"show",required_argument,0,0},
+		};
+
+		c = getopt_long(argc, argv, "a:bimrs:", long_options, &option_index);
+		if (c == -1)
+			break;
+		printf ("%d %d\n",c,option_index);
 		switch (c) {
+/*		case '0': */
+			/* If this option set a flag, do nothing else now. */
+/*			if (long_options[option_index].flag != 0)
+				break;
+			printf ("option %s", long_options[option_index].name);
+			if (optarg)
+				printf (" with arg %s", optarg);
+			printf ("\n");
+			break; */
 		case 'a': /* translate mem channel to memory address */
 			retrieveAddress(strdup(optarg));
 			exit(0);
@@ -44,6 +67,10 @@ main(int argc, char *argv[]) {
 			memInfo->printVerbose();
 			break;
 		default:
+			printf ("option %s", long_options[option_index].name);
+			if (optarg)
+				printf (" with arg %s", optarg);
+			printf ("\n");
 			usage(argv[0]);
 			break;
 		}
