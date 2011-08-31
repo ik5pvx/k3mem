7348c42c17808e435137954579eae769a028d11c
Pierfrancesco Caci <pf@tippete.net>
2011-08-31 14:59:12 +0200

    Adding license and copyright.

--- main.c
+++ main.c
@@ -1,3 +1,28 @@
+/* ----------------------------------------------------------------------------
+ *
+ * main.c
+ *
+ * Copyright (C) xxxx-2011
+ *		Mike Markowski, AB3AP
+ * Copyright (C) 2011-
+ *		Pierfrancesco Caci, IK5PVX
+ *
+ * This file is part of k3mem
+ *
+ * k3mem is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * k3mem is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with k3mem.  If not, see <http://www.gnu.org/licenses/>.
+ * ----------------------------------------------------------------------------
+ */
 #include <stdio.h>
 #include <string.h>
 #include <getopt.h>
